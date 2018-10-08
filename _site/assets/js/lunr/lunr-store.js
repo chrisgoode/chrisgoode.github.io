f333b06610d03c315751ba2369dc116d5141704a
Chris Goode <goodski@Chriss-MacBook-Pro.local>
2018-10-08 18:35:44 -0400

    ok

---
+++ _site/assets/js/lunr/lunr-store.js
@@ -0,0 +1,11 @@
+var store = [{
+        "title": "Welcome!",
+        "excerpt":"Hi. This page is an experiment in getting away from social media sites and managing my own content. Until I learn more about Jekyll and GitHub, this site is going to be a bit of a mess. Bear with me and you may be rewarded with thrilling stories of the...","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/First-Post/",
+        "teaser":null},{
+        "title": "Kevin!",
+        "excerpt":"Later this year (2018) I’ll be posting a tutorial on how to make an electric guitar using Fusion 360 and a CNC milling machine. The first step in all of this is to complete the excellent tutorial by EvanandKatelyn found here. Here’s some footage of our CNC putting the finishing...","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/Kevin/",
+        "teaser":null}]
